9e2fc2dd91e6a43dc0801e67e01a9dd9bedc3fd6
hanino87 <148004074+hanino87@users.noreply.github.com>
2024-09-25 09:23:51 +0200

    fixed code in the testfile with fewer code blocks for tests 1-3

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -11,28 +11,17 @@ import { APIPOSTS } from './apiPost';
 import { generateEmptyCustomerSchema } from './EmptyData';
 
 test.describe('Testsuite Hannes and Hampus Group', () => {
-  // declare all variables type notations before all test to avoid redundant code
   let apigets: APIGETS;
   let apiposts: APIPOSTS;
 
-  // hook before all test that initialise all new class object to their classes and with conected URL
   test.beforeAll(async ({  }) => {
     apigets = new APIGETS(GET_ALL_CUSTOMERS);
     apiposts = new APIPOSTS(ADD_CAR,ADD_CUSTOMER);
 
   })
 
-  test('Test case 01 find all customers', async ({ request }) => { // request här används som argumnet för att http anrop ska kunna göras inom testen 
-    // let apigets: APIGETS;
-    //This line doesn't initialize the variable,just declares that apigets will be of type APIGETS.
-    // apigets = new APIGETS(GET_ALL_CUSTOMERS);
-    /* The new APIGETS() syntax creates a new object of 
-    type APIGETS and assigns it to the variable apigets 
-    .*/
+  test('Test case 01 find all customers', async ({ request }) => { 
     const responseAllCustomers = await apigets.getAllCustomers(request);
-    /* asign a variabel to url allcustomes to object 
-    apigets and call metod getAllCustomers fom class apiGet
-    and with request run Apirequestcontext isolated on this test through request argument */
     expect(responseAllCustomers.ok()).toBeTruthy();
     expect(responseAllCustomers.status()).toBe(200);
     expect(responseAllCustomers.status()).not.toBe(500);
@@ -40,10 +29,6 @@ test.describe('Testsuite Hannes and Hampus Group', () => {
   });
 
   test('Test case 02 add cars with a registration number that are in the database', async ({ request, }) => { 
-    // let apiposts: APIPOSTS;
-    // //This line doesn't initialize the variable, just declares that apigets will be of type Class APIPOSTS.
-    // apiposts = new APIPOSTS(ADD_CAR,ADD_CUSTOMER);
-    // do a varaible paylod and asign it to metod generateFakerDataCreateCar
     const payload = generateFakerDataCreateCar();
     console.log(payload)
      // print out payload you send to the server url for add car for check paylod object with
@@ -54,15 +39,11 @@ test.describe('Testsuite Hannes and Hampus Group', () => {
   });
 
   test('Test case 03 add a customer without any value', async ({ request, }) => { 
-    // let apiposts: APIPOSTS;
-    // //This line doesn't initialize the variable, just declares that apigets will be of type Class APIPOSTS.
-    // apiposts = new APIPOSTS(ADD_CAR,ADD_CUSTOMER);
-    // do a varaible paylod and asign it to metod generateEmptyCustomerSchema
     const payload = generateEmptyCustomerSchema();
     console.log(payload)
      // print out payload you send to the server url for add customer for check paylod object with
     const responseCreateCustomerWithEmptyData = await apiposts.addCAR(request, payload);
-    // assert that you cant create a new car with a registration number that allready exists expect staus_code 409 
+    // assert that you can`t create a new customer with any information expect 400 and not 201 response. 
     expect(responseCreateCustomerWithEmptyData.status()).toBe(400);
     expect(responseCreateCustomerWithEmptyData.status()).not.toBe(201);
     // test fail you can add a customer with no information. Then the carfirm cant contact customers not god buisness. 
